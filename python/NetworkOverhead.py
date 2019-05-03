9a8e1984f6e44404c95ef703d0002f939d8689f7
yuyinghao <yuyinghao@yuyinghaos-MacBook-Pro.local>
2019-05-03 15:43:51 +0800

    network overhead test script

---
+++ python/NetworkOverhead.py
@@ -0,0 +1,33 @@
+
+import numpy
+import os
+import sys
+from os.path import dirname
+import subprocess
+import time
+
+'''
+Measure the network overhead with different partition numbers from 1-30
+'''
+
+
+def NetworkOverhead(totalCount):
+
+
+    tests_dir = os.path.expanduser('~') # for Linux
+    #tests_dir = os.getcwd() # for mac OS
+    print "tests dir:" + tests_dir
+
+    #fw = open(tests_dir+"/test_files/popularity.txt", "r")
+
+
+    #print popularity
+    for fileId in range(1, 31):
+        for i in range(0, totalCount):
+            # get a file id from the popularity
+            os.system('bin/alluxio runSPReadExecutor %s' % fileId)
+
+    os.system('echo "All read requests submitted!" ')
+
+if __name__ == "__main__":
+    SPBenchmark(int(sys.argv[1]))
