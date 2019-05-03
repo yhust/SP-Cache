15db9fc2e787fb05ec0e2c7cdfda8b6bd004785c
yuyinghao <yuyinghao@yuyinghaos-MacBook-Pro.local>
2019-05-03 15:22:24 +0800

    chmod

--- flintrock_scripts/wondershaper-install.sh
+++ flintrock_scripts/wondershaper-install.sh
@@ -2,7 +2,7 @@
 
 clustername=$1
 
-flintrock run-command $clustername "sudo yum -y install tc iper3; git clone  https://github.com/magnific0/wondershaper.git; cd wondershaper; sudo make install;"
+flintrock run-command $clustername "yum -y install tc iper3; git clone  https://github.com/magnific0/wondershaper.git; cd wondershaper;make install;"
 
 exit 0
 
--- flintrock_scripts/wondershaper-set.sh
+++ flintrock_scripts/wondershaper-set.sh
@@ -3,7 +3,8 @@
 clustername=$1
 bandwidth=$2  #kbps
 
-flintrock run-command $clustername "sudo wondershaper -a eth0 -u $bandwidth -d $bandwidth"
+flintrock run-command $clustername "sudo wondershaper -c -a eth0;
+sudo wondershaper -a eth0 -u $bandwidth -d $bandwidth"
 
 exit 0
 
