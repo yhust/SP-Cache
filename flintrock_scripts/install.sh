5e69eb9ed5814355b24638ee9b9dde7c2e58684c
yuyinghao <yuyinghao@yuyinghaos-MacBook-Pro.local>
2019-05-07 11:03:46 +0800

    scripts for wondershaper

--- flintrock_scripts/install.sh
+++ flintrock_scripts/install.sh
@@ -5,7 +5,7 @@
 clustername=$1
 
 #install necessary packages
-#flintrock run-command $clustername 'sudo yum update -y;sudo yum install java-1.8.0-openjdk* -y;export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk";sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo;sudo sed -i s/\$releasever/7/g /etc/yum.repos.d/epel-apache-maven.repo;sudo yum -y install apache-maven;sudo yum -y install git;sudo yum -y install python2-pip;sudo yum -y install gcc-c++;'
+flintrock run-command $clustername 'sudo yum update -y;sudo yum install java-1.8.0-openjdk* -y;export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk";sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo;sudo sed -i s/\$releasever/7/g /etc/yum.repos.d/epel-apache-maven.repo;sudo yum -y install apache-maven;sudo yum -y install git;sudo yum -y install python2-pip;sudo yum -y install gcc-c++;sudo pip install numpy'
 
 
 # download sp-cache
--- flintrock_scripts/wondershaper-install.sh
+++ flintrock_scripts/wondershaper-install.sh
@@ -2,7 +2,7 @@
 
 clustername=$1
 
-flintrock run-command $clustername "yum -y install tc iper3; git clone  https://github.com/magnific0/wondershaper.git; cd wondershaper;make install;"
+flintrock run-command $clustername "sudo yum -y install tc iperf3; sudo rm -r wondershaper; git clone  https://github.com/magnific0/wondershaper.git; cd wondershaper;sudo make install;"
 
 exit 0
 
